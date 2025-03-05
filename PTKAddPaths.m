1c78fb16f8b06e0783a8d405018041ab5e285722
Fung Chan <fung.chan@auckland.ac.nz>
2025-03-05 16:15:01 +1300

    ggremesh files

--- PTKAddPaths.m
+++ PTKAddPaths.m
@@ -77,6 +77,11 @@ function PTKAddPaths(varargin)
         path_folders{end + 1} = fullfile('External', 'gerardus', 'matlab', 'PointsToolbox');
         path_folders{end + 1} = fullfile('External', 'stlwrite');
         path_folders{end + 1} = fullfile('External', 'plywrite');
+        path_folders{end + 1} = fullfile('External', 'ggremesh');
+        path_folders{end + 1} = fullfile('External', 'ggremesh', 'geogram');
+        path_folders{end + 1} = fullfile('External', 'ggremesh', 'geogram', 'lin64');
+        path_folders{end + 1} = fullfile('External', 'ggremesh', 'geogram', 'mac64');
+        path_folders{end + 1} = fullfile('External', 'ggremesh', 'geogram', 'win64');
         path_folders{end + 1} = fullfile('External', 'npReg');
         path_folders{end + 1} = fullfile('External', 'depmat');
         path_folders{end + 1} = fullfile('External', 'MatlabWebSocket', 'src');
